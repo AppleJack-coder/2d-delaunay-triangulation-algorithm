5d8d107de8c611330e2d948c555926104a22e08b
AppleJack-coder <mirumian999@gmail.com>
2022-04-06 23:26:08 +0300

    Added class for testing new classes and methods

---
+++ Playground.java
@@ -0,0 +1,8 @@
+// This class is for testing other new classes
+
+public class Playground {
+    public static void main(String[] args) {
+        GenInitData genInitData = new GenInitData(100, 200, 8, 8);
+        genInitData.gen_points_coords();
+    }
+}
